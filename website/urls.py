7f06f986fb0de760259a1a920c14724a31c3c639
ron-ligsay <akyligsay16@gmail.com>
2023-08-27 01:21:10 +0800

    adding static

--- website/urls.py
+++ website/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for course_u project.
+URL configuration for dcrm project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -14,12 +14,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path, include
-#from django.conf import settings
+from . import views
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('website.urls')),
-    #path('',r'^images/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT,}),
+    path('', views.home, name='home'),
+    path('login/', views.login, name='login'),
 ]
