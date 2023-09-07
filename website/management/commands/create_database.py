dfca31bd49f7b433e8eb0105efd0923393081869
ron-ligsay <akyligsay16@gmail.com>
2023-09-07 20:05:40 +0800

    command load sql

--- website/management/commands/create_database.py
+++ website/management/commands/create_database.py
@@ -1,3 +1,4 @@
+from django.core.management.base import BaseCommand, CommandError
 import mysql.connector
 from decouple import Config, RepositoryEnv
 config = Config(RepositoryEnv('.env'))
@@ -6,14 +7,17 @@ config = Config(RepositoryEnv('.env'))
 # change user and password to your username and password respectively
 dataBase = mysql.connector.connect( host=config('DB_HOST', default='localhost'), user = config('DB_USER'), passwd = config('DB_PASS'), auth_plugin='mysql_native_password' )
 
-# prepare a cursor object
-cursorObject = dataBase.cursor()
 
-# Create a database, dcrn_db will be the name of your database (you can change this)
-cursorObject.execute("CREATE DATABASE CourseU_DB")
+class Command(BaseCommand):
+    help = 'Create the database'
 
-print("Database created successfully")
+    def handle(self, *args, **kwargs):
+        with dataBase.cursor() as cursor:
+            # Create a database, dcrn_db will be the name of your database (you can change this)
+            cursor.execute("CREATE DATABASE CourseU_DB")
+        self.stdout.write(self.style.SUCCESS("Database created successfully"))
 
+  
 
 # To run this program, open a terminal window and type:
 # python3 mydb.py
