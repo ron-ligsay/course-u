624f0f19e574ee3945bcc83210ee24e3a00cb183
ron-ligsay <akyligsay16@gmail.com>
2023-09-17 15:17:28 +0800

    added views

--- website/views.py
+++ website/views.py
@@ -4,7 +4,9 @@ from django.contrib import messages
 from .forms import SignUpForm
 from website.models import Specialization, Test
 from django.urls import reverse
-
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.views import LogoutView
+from django.shortcuts import render
 
 # Create your views here.
 def home(request):
@@ -70,3 +72,45 @@ def prev_test(request, pk):
         return render(request, 'test.html', {'test': test})
     else:
         return redirect('home')
+
+@login_required  # Ensure that the user is logged in to access the profile
+def user_profile(request):
+    user = request.user
+    # Query additional user profile data if using a custom user profile model
+    context = {'user': user}
+    return render(request, 'user_profile.html', context)
+
+
+def edit_profile(request):
+    user = request.user
+    # Query additional user profile data if using a custom user profile model
+    context = {'user': user}
+    return render(request, 'user_profile.html', context)
+
+def terms_and_conditions(request):
+    user = request.user
+    # Query additional user profile data if using a custom user profile model
+    context = {'user': user}
+    return render(request, 'user_profile.html', context)
+
+def settings(request):
+    user = request.user
+    # Query additional user profile data if using a custom user profile model
+    context = {'user': user}
+    return render(request, 'user_profile.html', context)
+
+class CustomLogoutView(LogoutView):
+    template_name = 'custom_logout.html'  # Optionally, specify a custom logout template
+
+    def get_next_page(self):
+        # Customize the redirection logic if needed
+        next_page = super().get_next_page()
+        # You can add additional logic here if required
+        return next_page
+
+def test_home(request):
+    return render(request, 'test_home.html')
+
+
+def view_test_results(request):
+    return render(request, 'test_home.html')
