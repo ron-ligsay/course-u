d38fef4910844769506f2a68d2e9bdf740c8fc5a
ron-ligsay <akyligsay16@gmail.com>
2023-09-21 13:35:08 +0800

    moved back lol

--- course_u/settings.py
+++ course_u/settings.py
@@ -63,7 +63,7 @@ ROOT_URLCONF = 'course_u.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR  / 'src/templates'],
+        'DIRS': [BASE_DIR  / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -133,8 +133,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'src/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'src/static')]
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
