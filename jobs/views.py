dc70e58c162c4c7aff74925962d902f4eda92b26
ron-ligsay <akyligsay16@gmail.com>
2023-09-21 13:55:54 +0800

    make jobs working

---
+++ jobs/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from .models import JobPosting
+
+# Create your views here.
+#########################################################################
+# ----------------------------for job--------------------------------- #
+#########################################################################
+# def job_list(request):
+#     job_postings = JobPosting.objects.all()
+#     return render(request, 'job_list.html', {'job_postings': job_postings})
+
+def job_list(request, job_id=None):
+    job_postings = JobPosting.objects.all()
+    selected_job = None
+
+    if job_id:
+        selected_job = get_object_or_404(JobPosting, pk=job_id)
+
+    return render(request, 'job/job_list.html', {'job_postings': job_postings, 'selected_job': selected_job})
+
+def job_detail(request, job_id):
+    job_posting = JobPosting.objects.get(pk=job_id)
+    return render(request, 'job/job_detail.html', {'job_posting': job_posting})
