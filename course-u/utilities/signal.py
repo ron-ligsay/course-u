f08e971cff3c4d46780230a42652c8706cb51ffd
ron-ligsay <akyligsay16@gmail.com>
2023-10-20 09:49:29 +0800

    using templates for emails

--- course-u/utilities/signal.py
+++ course-u/utilities/signal.py
@@ -17,11 +17,22 @@ my_signal.send(sender="my_sender")
 from django.core.mail import send_mail
 from django.contrib.auth.models import User
 from django.conf import settings
+from django.template.loader import render_to_string
 
 def send_welcome_email(sender, **kwargs):
     user = sender  # The sender will be a User object
-    subject = 'Welcome to Our Application'
-    message = f'Hello, {user.username}! Thank you for registering.'
+    #subject = 'Welcome to Our Application'
+    #message = f'Hello, {user.username}! Thank you for registering.'
+
+    # Render the subject and message templates with user-specific context
+    subject = render_to_string('email/welcome_email_subject.txt')
+    message = render_to_string('email/welcome_email_message.txt', {'user': user})
+
     from_email = settings.DEFAULT_FROM_EMAIL
     recipient_list = [user.email]
-    send_mail(subject, message, from_email, recipient_list)
+    send_mail(
+        subject, 
+        message, 
+        from_email, # From email address
+        recipient_list # Recipient(s)
+    )
