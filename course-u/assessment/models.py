8dc313688c221a76692d94da0ec02831564a15a7
ron-ligsay <akyligsay16@gmail.com>
2023-10-27 15:06:20 +0800

    creating models

--- course-u/assessment/models.py
+++ course-u/assessment/models.py
@@ -43,3 +43,41 @@ class UserResponse(models.Model):
         return f"Set: {self.set.set_id}, Question: {self.question_id}"
     class Meta:
         unique_together = ('question','set') 
+
+class MBTI(models.Model):
+    mbti = models.AutoField(primary_key=True,serialize=False, auto_created=True)
+    mbti_question = models.CharField(max_length=1000)
+    option_a = models.CharField(max_length=1000)
+    option_b = models.CharField(max_length=1000)
+    ans_a = models.CharField(max_length=15)
+    ans_b = models.CharField(max_length=15)
+    acr_a = models.CharField(max_length=1)
+
+    def __str__(self):
+        return f"{self.mbti_question} ({self.ans_a}/{self.ans_b})"
+    
+class MBTISet(models.Model):
+    mbti_set_id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    is_completed = models.BooleanField(default=False)
+    # float values 1 - 5
+    mind = models.FloatField(default=0)
+    energy = models.FloatField(default=0)
+    nature = models.FloatField(default=0)
+    tactics = models.FloatField(default=0)
+    # identity is the personality type
+    identity = models.CharField(max_length=5, default="")
+
+    def __str__(self):
+        return f"{self.mbti_set_id}: Status : {self.is_completed} by User: {self.user} (Identity: {self.identity}) "
+    
+
+class MBTIResponse(models.Model):
+    mbti_response_id = models.AutoField(primary_key=True,serialize=False, auto_created=True)
+    mbti_set = models.ForeignKey(MBTISet, on_delete=models.CASCADE, null=True, blank=True)
+    mbti = models.ForeignKey(MBTI, on_delete=models.CASCADE)
+    is_answered = models.BooleanField(default=False)
+    selected_option = models.CharField(max_length=1)
+
+    def __str__(self):
+        return f"Set: {self.mbti_set.mbti_set_id}, Question: {self.mbti_response_id}"
