982530c04d49eb4d8dd7cf97907ced6fab42cfa4
ron-ligsay <akyligsay16@gmail.com>
2023-11-01 16:06:30 +0800

    plots utils

---
+++ course-u/utilities/plots.py
@@ -0,0 +1,14 @@
+import plotly.express as px
+from plotly.offline import plot
+
+def generate_pie_chart(data, title):
+    fig = px.pie(
+        values=list(data.values_list("total_correct", flat=True)),
+        names=list(data.values_list("field_name", flat=True)),
+        title=title
+    )
+    fig.update_traces(textposition='inside', textinfo='percent+label')
+    fig.update_layout(uniformtext_minsize=12, uniformtext_mode='hide')
+    
+    return fig
+
