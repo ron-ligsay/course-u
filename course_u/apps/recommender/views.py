da181d80c4084b4411ca21d4270f94f422fce188
ron-ligsay <akyligsay16@gmail.com>
2023-11-14 22:18:23 +0800

    recommender view

--- course_u/apps/recommender/views.py
+++ course_u/apps/recommender/views.py
@@ -1,107 +1,121 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 from django.contrib.auth.models import User
+
 from .models import UserSkill
-
-from apps.website.models import Skill, Specialization, SpecializationSkills
-
+from apps.website.models import Skill, Specialization, SpecializationSkills, Field
 
 import pandas as pd
 from sklearn.metrics.pairwise import cosine_similarity
 
-from scipy.sparse import csr_matrix
-
-#from .utils import load_ml_model, make_prediction
-
-# get user skills
-# calculate similarity
-# get top 5 similar users
-# save top 5 similar users in a list
-# update user profile with top 5 similar users
-# user interaction
-# save user interaction in a list
-# get user interaction list
-
-def get_normalized_skill_levels_df(request):
-    """Returns a DataFrame with the index of the specialization name, the columns of the user skills, and the values of the skill levels, where the values are normalized by dividing the sum of the skills of the specialization."""
-
-    # Get a list of all specialization names
-    specialization_names = [
-        s.title
-        for s in Specialization.objects.all()
-    ]
-
-    # Get a list of all user skills
-    user_skills = UserSkill.objects.filter(user=request.user)
-    skill_list = [skill.skill for skill in user_skills]
-
-    # Create a DataFrame to store the skill levels for each specialization
-    skill_levels_df = pd.DataFrame(columns=skill_list)
-
-    # Iterate over the specializations
-    for specialization in Specialization.objects.all():
-
-        # Create a dictionary to store the skill levels for the specialization
-        skill_levels = {}
-
-        # Iterate over the user's skills
-        for skill in skill_list:
-
-            # Check if the skill is in the specialization's skill set
-            if skill in specialization.specializationskills_set.all():
-
-                # Get the skill level
-                skill_level = specialization.specializationskills_set.get(skill=skill).level
-
-            else:
-
-                # Set the skill level to 0
-                skill_level = 0
-
-            # Add the skill level to the dictionary
-            skill_levels[skill] = skill_level
-
-    # Calculate the sum of the skill levels for each specialization
-    sum_of_skill_levels = skill_levels_df.apply(lambda x: x.sum(), axis=1)
-
-    # Normalize the skill levels by dividing the sum of the skill levels for the specialization
-    skill_levels_df = skill_levels_df.apply(lambda x: x / sum_of_skill_levels, axis=1)
-
-    return skill_levels_df
+import os
 
 
-def get_top_3_specialization_recommendations(request):
-    """Returns the top 3 specializations with the highest cosine similarity to the user's skill levels."""
+def load_csv(request):
+    # Assuming your CSV file is in the same folder as your views
+    csv_file_name = 'specialization_sparse.csv'
+    csv_file_path = os.path.join(os.path.dirname(__file__), csv_file_name)
 
-    # Get the normalized skill levels DataFrame
-    skill_levels_df = get_normalized_skill_levels_df(request)
+    try:
+        # Read the CSV file into a DataFrame
+        specialization_sparse = pd.read_csv(csv_file_path)
 
-    # Add the User row to the skill levels DataFrame
-    skill_levels_df.loc["User"] = UserSkill.objects.filter(user=request.user).values_list("skill", "level")
-    
-    # Append the user skill levels DataFrame to the skill levels DataFrame
-    #skill_levels_df = pd.concat([skill_levels_df, user_skill_levels_df], axis=0)
+        # Process the DataFrame as needed (e.g., save to the database, perform operations)
+        # ...
+        print("specialization_sparse: ", specialization_sparse)
+        return specialization_sparse
+    except Exception as e:
+        print("Error processing file: {}".format(e))
+        # empty DataFrame with the same columns and dtypes as your original DataFrame
+        
+        specialization_sparse = pd.DataFrame()
 
-    # Calculate the cosine similarity between the user's skill levels and the specialization skill levels
-    cosine_similarity_matrix = cosine_similarity(skill_levels_df.loc["User"].reshape(1, -1), skill_levels_df.drop("User", axis=0))
-
-    # Get the top 3 specializations with the highest cosine similarity to the user's skill levels
-    top_3_specializations = skill_levels_df.drop("User", axis=0).index[cosine_similarity_matrix.argmax(axis=1)].tolist()[:3]
-
-    return top_3_specializations
+        return specialization_sparse
 
 
 
 def recommender(request):
+    # Get the user's skills from the request.
+    user_skills = UserSkill.objects.filter(user=request.user)
 
-    # Get the top 3 specialization recommendations
-    top_3_specialization_recommendations = get_top_3_specialization_recommendations(request)
+    # Get a list of user skill titles
+    user_skills_list = [skill.skill.skill for skill in user_skills]
 
-    # Print the top 3 specialization recommendations
-    print("The top 3 specialization recommendations are:")
-    for specialization in top_3_specialization_recommendations:
-        print(specialization)
+    # unique user skills
+    user_skills_list = list(set(user_skills_list))
+    print('user_skills_list: ', user_skills_list)
+
+    # convert to list
+    user_skills_list = list(user_skills_list)
+    print("type: ", type(user_skills_list))
+
+    # get level of each skill
+    user_skills_level = []
+    for skill in user_skills_list:
+        user_skills_level.append(UserSkill.objects.filter(user=request.user, skill__skill=skill).first().level)
+    print('user_skills_level: ', user_skills_level)
+
+    # create a dictionary of user skills and levels, 
+    user_skills_dict = dict(zip(user_skills_list, user_skills_level))
+    
+    # convert the dictionary to a dataframe, where the columns are the skills and the values are the levels
+    user_skills_df = pd.DataFrame.from_dict(user_skills_dict, orient='index').T
+
+
+    # fill the missing values with 0
+    user_skills_df = user_skills_df.fillna(0)
+
+    # Normalize the user's skills. by getting the sum of the user skills and dividing each skill by the sum. if skill = 0, then = 0
+    normalized_user_skills_df = user_skills_df / user_skills_df.sum(axis=1, skipna=True)
+
+    # fill the missing values with 0
+    normalized_user_skills_df = normalized_user_skills_df.fillna(0)
+    print('normalized_user_skills_df: ', normalized_user_skills_df)
+    
+    specialization_sparse = load_csv(request)
+
+    # Filter the specialization data frame columns by the user's skills. except the first and second columns
+    specialization_sparse_filtered = specialization_sparse[['title','field_id'] + user_skills_list]
+    # remove rows with all 0
+    specialization_sparse_filtered = specialization_sparse_filtered[(specialization_sparse_filtered[user_skills_list] != 0).any(axis=1)]
+
+
+    print('specialization_sparse_filtered: ', specialization_sparse_filtered)
+
+    cosine_similarities = cosine_similarity(normalized_user_skills_df[user_skills_list], specialization_sparse_filtered[user_skills_list])
+    top_3_indices = cosine_similarities.argsort(axis=1)[:, -3:]
+    top_3_specializations = specialization_sparse_filtered.iloc[:, 0].values[top_3_indices]
+    print('top_3_specializations: ', top_3_specializations)
+
+    
+    # get unique field_id
+    field_ids = specialization_sparse_filtered['field_id'].unique()
+    
+    # Calculate the sum of the cosine similarity scores for each skill in each field.
+    fields_score = {}
+    for field_id in field_ids:
+        # filter specialization_sparse_filtered by field_id
+        specialization_sparse_filtered_by_field = specialization_sparse_filtered[specialization_sparse_filtered['field_id'] == field_id]
+        cosine_similarities = cosine_similarity(normalized_user_skills_df[user_skills_list], specialization_sparse_filtered_by_field[user_skills_list])
+        # get total sum score of each field and add to field_score
+        fields_score[field_id] = cosine_similarities.sum(axis=1).sum()
+
+    # Sort the fields by the sum of the cosine similarity scores, in descending order.
+    top_3_fields = sorted(fields_score, key=fields_score.get, reverse=True)[:3]
+    # top fields score
+    top_3_fields_score = [fields_score[field_id] for field_id in top_3_fields]
+    print('top_3_fields: ', top_3_fields)
+    print('top_3_fields_score: ', top_3_fields_score)
+    # Get the field names for the top 3 fields.
+    field_names = []
+    for field_id in top_3_fields:
+        field_name = Field.objects.get(field=field_id).field_name
+        field_names.append(field_name)
+
+
 
     return render(request, 'recommender/recommender.html', {
-            #'skill_levels_df': skill_levels_df
-        })
+        'top_3_specialization_recommendations': top_3_specializations,
+        'field_names': field_names,
+    })
