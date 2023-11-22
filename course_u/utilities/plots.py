e709da85a233ccde286b63af6112935fc42a983b
ron-ligsay <akyligsay16@gmail.com>
2023-11-23 05:26:45 +0800

    update test results design

--- course_u/utilities/plots.py
+++ course_u/utilities/plots.py
@@ -5,7 +5,9 @@ def generate_pie_chart(data, title):
     fig = px.pie(
         values=list(data.values_list("total_correct", flat=True)),
         names=list(data.values_list("field_name", flat=True)),
-        title=title
+        #title=title,
+        height=500,
+        width=600,
     )
     fig.update_traces(textposition='inside', textinfo='percent+label')
     fig.update_layout(uniformtext_minsize=12, uniformtext_mode='hide')
